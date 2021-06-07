64df2b90cf06e7e9d713e5c81e2a45b3813b2f16
Lukas Hroch <lukashroch@gmail.com>
2021-06-07 18:18:49 +0100

    feat(util): random string generator

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './duo-exception';
+export * as constants from './constants';
+export * as util from './util';
---
+++ src/util.ts
@@ -0,0 +1,11 @@
+export const generateRandomString = (length: number): string => {
+  const result = [];
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+  }
+
+  return result.join('');
+};
